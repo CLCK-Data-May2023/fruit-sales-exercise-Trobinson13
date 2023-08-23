fda1fbb00f1cca8948dc1c44a92d71befc966f8a
Trobinson13 <trerrobinson@gmail.com>
2023-08-23 19:07:24 -0400

    Uploading code for fruitsales

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+# import denpendecies
+import pandas as pd
+
+# create/import dataframe
+data = {
+    'Apples': [35, 41],
+    'Bananas': [21, 34]
+}
+
+df = pd.DataFrame(data, index = ['2017 Sales', '2018 Sales'])
+
+# export data frame to csv file 
+df.to_csv('fruit.csv')
